b57b51375ef8850863a5e828839f0df177cfb035
Geoff Davis <geoff@geoffdavis.com>
2025-12-15 08:39:46 -0800

    refactor(tls): remove redundant HTTPS security group rule from certificates configuration
    The HTTPS ingress rule for the ALB security group was duplicated in the certificates.tf file. This rule is likely already defined in the main security group configuration, making this declaration redundant.

--- terraform/certificates.tf
+++ terraform/certificates.tf
@@ -27,14 +27,3 @@ resource "aws_lb_listener" "public_https" {
 
   tags = local.common_tags
 }
-
-# Update security group to allow HTTPS
-resource "aws_security_group_rule" "alb_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public_alb.id
-  description       = "Allow HTTPS from internet"
-}
